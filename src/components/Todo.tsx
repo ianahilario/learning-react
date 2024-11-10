6318fc54e9e3efb531f0fb2dc503b6f82dc3c563
ianahilario <iana.hilario@gmail.com>
2024-11-10 17:24:02 +0800

    demo app

---
+++ src/components/Todo.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Todo } from "../App";
+
+/* create the prop types by merging the Todo types and other expected props from the App component */
+type TodoProps = Todo & {
+  deleteTodo: (id: number) => void,
+  toggleDone: (id: number) => void,
+};
+
+//define the Todo Component
+export const TodoComponent = ({
+  id,
+  todo,
+  isDone,
+  deleteTodo,
+  toggleDone,
+}: TodoProps) => {
+  return (
+    <li
+      className={isDone ? "todo done" : "todo"}
+      onClick={() => toggleDone(id)}
+    >
+      <div>
+        <p>{todo}</p>
+      </div>
+      <button onClick={() => deleteTodo(id)}>delete</button>
+    </li>
+  );
+};
